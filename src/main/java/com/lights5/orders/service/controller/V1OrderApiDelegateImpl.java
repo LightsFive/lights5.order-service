1bb178e7a88402b9cc2bde173b02a4e221374072
NikhilLella <nikhileswarlella@gmail.com>
2023-01-21 00:49:42 +0530

    update

---
+++ src/main/java/com/lights5/orders/service/controller/V1OrderApiDelegateImpl.java
@@ -0,0 +1,11 @@
+package com.lights5.orders.service.controller;
+
+
+import com.lights5.orders.service.api.V1ApiDelegate;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+public class V1OrderApiDelegateImpl implements V1ApiDelegate {
+
+
+}
