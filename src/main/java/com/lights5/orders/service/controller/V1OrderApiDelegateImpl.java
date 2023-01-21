5be41bdccad60e4f6c459721ee97fd1c6119d0c0
NikhilLella <nikhileswarlella@gmail.com>
2023-01-21 23:03:39 +0530

    add delegate

--- src/main/java/com/lights5/orders/service/controller/V1OrderApiDelegateImpl.java
+++ src/main/java/com/lights5/orders/service/controller/V1OrderApiDelegateImpl.java
@@ -2,10 +2,13 @@ package com.lights5.orders.service.controller;
 
 
 import com.lights5.orders.service.api.V1ApiDelegate;
-import org.springframework.http.HttpStatusCode;
+import com.lights5.orders.service.model.Order;
 import org.springframework.http.ResponseEntity;
 
 public class V1OrderApiDelegateImpl implements V1ApiDelegate {
 
-
+    @Override
+    public ResponseEntity<Order> getOrdersOrderId(String orderId) {
+        return V1ApiDelegate.super.getOrdersOrderId(orderId);
+    }
 }
